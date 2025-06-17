b40b5fd3168855a12ba5577f7209b9b06dfcdfcf
an.hhm <an.hhm@vgamestd.coman.hhm@vgamestd.com>
2025-06-17 17:22:51 +0700

    feat: contain duplicate

---
+++ src/main/java/com/example/leet_code_camp/ContainDuplicate/ContainDuplicate.java
@@ -0,0 +1,31 @@
+package com.example.leet_code_camp.ContainDuplicate;
+
+import java.util.HashSet;
+
+public class ContainDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (Integer i : nums) {
+            if (seen.contains(i)) {
+                return true;
+            }
+
+            seen.add(i);
+        }
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainDuplicate checker = new ContainDuplicate();
+
+        int[] nums1 = {1, 2, 3, 4};
+        int[] nums2 = {1, 2, 3, 1};
+
+        System.out.println("nums1 has duplicate: " + checker.containsDuplicate(nums1)); // false
+        System.out.println("nums2 has duplicate: " + checker.containsDuplicate(nums2)); // true
+    }
+}
