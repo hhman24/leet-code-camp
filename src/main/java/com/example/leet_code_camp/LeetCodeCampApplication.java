d52e0deb9dc41b34c45d94abec9ad65ac5d4e63a
an.hhm <an.hhm@vgamestd.coman.hhm@vgamestd.com>
2025-06-17 16:53:53 +0700

    feat: init idea

---
+++ src/main/java/com/example/leet_code_camp/LeetCodeCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.leet_code_camp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetCodeCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leet_code_camp/LeetCodeCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leet_code_camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
